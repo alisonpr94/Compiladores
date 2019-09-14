532fc1f9f248adfd61b7d8beb24a7a8b8fa3cf9d
Alison Ribeiro <alisonrib17@gmail.com>
2019-09-14 11:47:22 -0300

    primeiro commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from analizador_lexico import *
+from analizador_sintatico import *
+from analizador_semantico import *
+from utils import *
+
+def main():
+	
+
+if __name__ == '__main__':
+	main()
